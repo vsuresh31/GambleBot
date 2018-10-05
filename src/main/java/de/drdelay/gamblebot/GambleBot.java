12ad6a7923312299bf84c6ee5a47e65dacf40eef
DrDelay <info@vi0lation.de>
2018-10-05 21:59:52 +0200

    config.txt: Skip empty lines

--- src/main/java/de/drdelay/gamblebot/GambleBot.java
+++ src/main/java/de/drdelay/gamblebot/GambleBot.java
@@ -301,6 +301,10 @@ public class GambleBot implements AbortsOnEsc {
         try {
             try (BufferedReader br = new BufferedReader(new FileReader("config.txt"))) {
                 for (String line; (line = br.readLine()) != null; ) {
+                    line = line.trim();
+                    if (line.length() == 0) {
+                        continue;
+                    }
                     if (line.equals("-")) {
                         fill = suffixes;
                         continue;
